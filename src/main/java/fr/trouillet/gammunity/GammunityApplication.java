1c52d8183e1ccbfc5f89cb104442b8640992454e
Damien Trouillet <contact@damien-trouillet.fr>
2020-04-18 17:20:15 +0200

    Init project

---
+++ src/main/java/fr/trouillet/gammunity/GammunityApplication.java
@@ -0,0 +1,13 @@
+package fr.trouillet.gammunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GammunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GammunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/trouillet/gammunity/GammunityApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.trouillet.gammunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GammunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
